34cab441f388d603a0f6e33697002e84a0f1aa62
Otterton1978 <yohei@hashicorp.com>
2021-03-22 12:26:02 +1100

    remote token

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 provider "vault" {
-    address = "https://loving-loon.hc.yk-vault.ml:8200/"
-    token = "s.Rz6kra6zwOofnMvnASOoklAV"
-}
-
-resource "vault_mount" "kv-v2-root" {
-    path = "kv-v2-root"
-    type = "kv-v2"
-    description = "Example mount created by terraform"
+    # Use VAULT_ADDR as an environment variable to specify the address of vault server
+    # Use VAULT_TOKEN as an environment variable to specify the token to be used
 }
